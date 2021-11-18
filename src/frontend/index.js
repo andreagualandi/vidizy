7cbda084d43a06e24e90d6f775f51e31697087a1
Andrea <gorgonauta84@yahoo.it>
2021-11-18 18:13:50 +0100

    switch to sveltejs

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,20 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { MemoryRouter } from 'react-router-dom';
-import './global.less';
-import App from './components/App';
+import App from './App.svelte';
 
-document.addEventListener('dragover', (e) => {
-    e.preventDefault();
-    return false;
-}, false);
+const app = new App({
+    target: document.body,
+    props: {
+        name: 'world'
+    }
+});
 
-document.addEventListener('drop', (e) => {
-    e.preventDefault();
-    return false;
-}, false);
-
-ReactDOM.render(
-    <MemoryRouter>
-        <App />
-    </MemoryRouter> , document.getElementById('root'));
+export default app;
